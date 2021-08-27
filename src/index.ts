b8a35e32697c6c81fbd0fae1413070f4c1572b58
warstrolo <warstrolo.wizard@gmail.com>
2021-08-27 17:46:35 +0200

    initial update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // dependency import
 
-import aes256 from "aes256"
+import Cryptr from "cryptr"
 import fs from "fs"
 import  { promisify } from "util"
 
@@ -8,16 +8,28 @@ import  { promisify } from "util"
 
 
 const readdir = promisify(fs.readdir) // promisify the fs.readdir 
+const readfile = promisify(fs.readFile) // promisify the fs.readFile
+const writefile = promisify(fs.writeFile) // promisify the fs.writeFile
 const randomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 const randomcharlengh = randomChar.length;
 
 /** Read dir function
 * @param {string} directory take the directory
 * @return {Promise<string[]>} return a promise of files path
+* Will be use when encrypt dir feature will be created
 */
 async function readDir(directory : string) : Promise<string[]> {
   let files = await readdir(directory)
   return files.map(file => `${directory}/${file}`)
+}
+
+/** Read file function
+* @param {string} path take the path
+* @return {Promise<string>} return promise of data
+*/
+async function readFile(path : string) : Promise<string> {
+  let files : Buffer = await readfile(path)
+  return files.toString()
 }
 
 /** RandomString function 
@@ -33,14 +45,35 @@ function RandomString(length : number) : string {
 }
 
 /** CryptFile function 
-* @param {number} length take the number of element in your string
-* @return {string} Return the random string 
+* @param {string} path take the path of the input file
+* @param {string} dest take the path of the destination file
+* @param {string | undefined} key take the key if none specified one will be generated 
+* @return {string} Return the key
 */
-function CryptFile() : string {
-  var result : string
-  for ( var i = 0; i < length; i++ ) {
-    result += randomChar.charAt(Math.floor(Math.random() * randomcharlengh));
+async function CryptFile(path : string, dest : string, key? : string) : Promise<string> {
+  let enckey = key
+  if (!enckey) {
+    enckey = RandomString(20)
   }
-  return result
+  let crypt = new Cryptr(enckey)
+  const data = await readFile(path)
+  const dataenc = crypt.encrypt(data)
+  await writefile(dest, dataenc)
+  return enckey
+}
+
+/** CryptFile function 
+* @param {string} path take the path of the input file
+* @param {string} dest take the path of the destination file
+* @param {string} key take the secret key
+* @return {string} Return the key
+*/
+async function DecryptFile(path : string, dest : string, key : string) : Promise<string> {
+  let enckey = key
+  let crypt = new Cryptr(enckey)
+  const data = await readFile(path)
+  const dataenc = crypt.decrypt(data)
+  await writefile(dest, dataenc)
+  return enckey
 }
 
